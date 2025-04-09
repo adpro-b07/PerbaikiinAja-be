cf09ccebd801d138a56d83131d1078756e3aa6e1
Muhammad Firaz Al Aqib <mfirazalaqib@gmail.com>
2025-04-09 09:21:06 +0700

    Init Springboot project

---
+++ src/main/java/com/advprog/perbaikiinaja/PerbaikiinAjaApplication.java
@@ -0,0 +1,13 @@
+package com.advprog.perbaikiinaja;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PerbaikiinAjaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PerbaikiinAjaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/advprog/perbaikiinaja/PerbaikiinAjaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.advprog.perbaikiinaja;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerbaikiinAjaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

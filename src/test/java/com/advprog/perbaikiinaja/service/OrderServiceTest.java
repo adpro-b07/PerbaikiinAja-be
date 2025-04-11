a513ae99c0cbcd7ce452e00770102db451f5b34d
MichelineWL <michelinewijaya@gmail.com>
2025-04-11 14:01:53 +0700

    [REFACTOR] Add technician service to order service implementation (final commit for order dummy before go to 'menerima konfirmasi dan pemesanan jasa' feature

--- src/test/java/com/advprog/perbaikiinaja/service/OrderServiceTest.java
+++ src/test/java/com/advprog/perbaikiinaja/service/OrderServiceTest.java
@@ -2,6 +2,7 @@ package com.advprog.perbaikiinaja.service;
 
 import com.advprog.perbaikiinaja.model.Order;
 import com.advprog.perbaikiinaja.repository.OrderRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
@@ -9,6 +10,8 @@ import org.mockito.Mock;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 public class OrderServiceTest {
@@ -16,16 +19,21 @@ public class OrderServiceTest {
     OrderRepository orderRepo;
     @InjectMocks
     OrderServiceImpl orderService;
+    @Mock TechnicianService technicianService;
 
-    @Test
-    void getOrdersForTechnician_shouldCallRepositoryAndReturnList() {
-        Order o1 = new Order(); o1.setTechnicianId("t1");
-        when(orderRepo.findByTechnicianId("t1")).thenReturn(List.of(o1));
-
-        List<Order> result = orderService.getOrdersForTechnician("t1");
-
-        assertEquals(1, result.size());
-        assertEquals("t1", result.get(0).getTechnicianId());
+    @BeforeEach
+    void setUp() {
+        orderRepo = mock(OrderRepository.class);
+        technicianService = mock(TechnicianService.class);
+        orderService = new OrderServiceImpl(orderRepo, technicianService);
     }
 
+    @Test
+    void getOrdersForTechnician_shouldThrowIfInactive() {
+        when(technicianService.isTechnicianActive("t1")).thenReturn(false);
+
+        assertThrows(IllegalStateException.class, () -> {
+            orderService.getOrdersForTechnician("t1");
+        });
+    }
 }

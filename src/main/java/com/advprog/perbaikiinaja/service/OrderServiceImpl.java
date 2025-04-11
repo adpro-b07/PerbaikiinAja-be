b3e505ab2aaf3e81821f7e82ee09f7cafe499689
MichelineWL <michelinewijaya@gmail.com>
2025-04-11 13:57:16 +0700

    [REFACTOR] Add technician service to order service implementation (final commit for order dummy before go to 'menerima konfirmasi dan pemesanan jasa' feature

--- src/main/java/com/advprog/perbaikiinaja/service/OrderServiceImpl.java
+++ src/main/java/com/advprog/perbaikiinaja/service/OrderServiceImpl.java
@@ -6,20 +6,23 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-
 @Service
 public class OrderServiceImpl implements OrderService {
 
     private final OrderRepository orderRepo;
+    private final TechnicianService technicianService;
 
     @Autowired
-    public OrderServiceImpl(OrderRepository orderRepo) {
+    public OrderServiceImpl(OrderRepository orderRepo, TechnicianService technicianService) {
         this.orderRepo = orderRepo;
+        this.technicianService = technicianService;
     }
 
     @Override
     public List<Order> getOrdersForTechnician(String technicianId) {
+        if (!technicianService.isTechnicianActive(technicianId)) {
+            throw new IllegalStateException("Technician is not active");
+        }
         return orderRepo.findByTechnicianId(technicianId);
     }
 }
-

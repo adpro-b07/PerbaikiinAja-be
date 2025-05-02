13a731ecb340c4dc226355a7102e7f2cf21796ed
MichelineWL <michelinewijaya@gmail.com>
2025-05-02 18:49:34 +0700

    [RED] unit test for report model

---
+++ src/test/java/com/advprog/perbaikiinaja/model/ReportTest.java
@@ -0,0 +1,31 @@
+package com.advprog.perbaikiinaja.model;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReportTest {
+
+    @Test
+    public void testConstructor() {
+        PaymentMethod method = new PaymentMethod("PM01", "Bank A");
+        Pesanan pesanan = new Pesanan("Kulkas", "Tidak dingin", null, "u@mail.com", "t@mail.com", method);
+        Report report = new Report("Bagus", 5, pesanan);
+
+        assertEquals("Bagus", report.getUlasan());
+        assertEquals(5, report.getRating());
+        assertEquals(pesanan, report.getPesanan());
+    }
+
+    @Test
+    public void testSetters() {
+        PaymentMethod method = new PaymentMethod("PM01", "Bank A");
+        Pesanan pesanan = new Pesanan("Kulkas", "Tidak dingin", null, "u@mail.com", "t@mail.com", method);
+        Report report = new Report("OK", 3, pesanan);
+
+        report.setUlasan("Mantap");
+        report.setRating(4);
+
+        assertEquals("Mantap", report.getUlasan());
+        assertEquals(4, report.getRating());
+    }
+}

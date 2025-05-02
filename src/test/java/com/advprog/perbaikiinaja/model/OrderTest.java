40c94c7292cb8a89f33da573918d168b354b9cba
andreast12 <andreast12.id@gmail.com>
2025-05-02 18:18:02 +0700

    [RED] add new tests for Order model

--- src/test/java/com/advprog/perbaikiinaja/model/OrderTest.java
+++ src/test/java/com/advprog/perbaikiinaja/model/OrderTest.java
@@ -11,4 +11,39 @@ public class OrderTest {
         assertEquals(OrderStatus.MENUNGGU_ESTIMASI, order.getStatus());
     }
 
+    @Test
+    void shouldSetAndGetRepairDescription() {
+        Order order = new Order();
+        order.setRepairDescription("Screen replacement");
+        assertEquals("Screen replacement", order.getRepairDescription());
+    }
+
+    @Test
+    void shouldSetAndGetServiceDate() {
+        Order order = new Order();
+        LocalDate date = LocalDate.of(2025, 5, 2);
+        order.setServiceDate(date);
+        assertEquals(date, order.getServiceDate());
+    }
+
+    @Test
+    void couponShouldBeOptional() {
+        Order order = new Order();
+        assertNull(order.getCoupon());
+        order.setCoupon("COUPON123");
+        assertEquals("COUPON123", order.getCoupon());
+    }
+
+    @Test
+    void shouldSetAndGetPaymentMethod() {
+        Order order = new Order();
+        PaymentMethod pm = PaymentMethod.builder()
+                .id("pm1")
+                .name("Credit Card")
+                .userId("user1")
+                .accountNumber("123456")
+                .isActive(true)
+                .build();
+        order.setPaymentMethod(pm);
+        assertEquals(pm, order.getPaymentMethod());
 }

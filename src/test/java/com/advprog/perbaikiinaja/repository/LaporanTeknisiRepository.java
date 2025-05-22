f42000df2a681fd2adf6a24aaf2a0cecfe677186
MichelineWL <michelinewijaya@gmail.com>
2025-05-22 13:23:13 +0700

    [RED] add test for laporan teknisi repository

---
+++ src/test/java/com/advprog/perbaikiinaja/repository/LaporanTeknisiRepository.java
@@ -0,0 +1,7 @@
+package com.advprog.perbaikiinaja.repository;
+
+import com.advprog.perbaikiinaja.model.LaporanTeknisi;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface LaporanTeknisiRepository extends JpaRepository<LaporanTeknisi, String> {
+}
